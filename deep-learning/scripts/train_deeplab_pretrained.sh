2d8d9adbe8563f3052f9ce03ef117c31ed1a4dd2
kaunild <dhruv.kaunil@gmail.com>
2020-02-20 10:15:46 -0700

    Update train_deeplab_pretrained.sh

--- deep-learning/scripts/train_deeplab_pretrained.sh
+++ deep-learning/scripts/train_deeplab_pretrained.sh
@@ -17,6 +17,6 @@ echo "== This is the scripting step! =="
 
 sleep 30
 # lr-model-momentum-weightdecay
-python /projects/kadh5719/image-segmentation_antarctica/deep-learning/scripts/train_deeplab_pretrained.py --lr 0.00001 --train-batch-size 64 --restart-checkpoint True --checkpoint-path /projects/kadh5719/image-segmentation_antarctica/deep-learning/models/completed_sessions/deeplabv3-s038-sgd-4-0.9-4-28.pth --checkpoint-prefix deeplabv3-sgd-4-0.9-4-28-sgd-4-0.9-4
+python /projects/kadh5719/image-segmentation_antarctica/deep-learning/scripts/train_deeplab_pretrained.py --lr 0.00001 --train-batch-size 64 --restart-checkpoint True --checkpoint-path /projects/kadh5719/image-segmentation_antarctica/deep-learning/models/completed_sessions/deeplabv3-s038-sgd-4-0.9-4-29.pth --checkpoint-prefix deeplabv3-sgd-4-0.9-4-29-sgd-4-0.9-4
 
 echo "== End of Job =="
