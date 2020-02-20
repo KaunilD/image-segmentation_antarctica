849a9a42d1b500daeb0d1f4da0c888b91db26edb
kaunild <dhruv.kaunil@gmail.com>
2020-02-20 10:03:10 -0700

    retrain session 2

--- deep-learning/scripts/train_deeplab_pretrained.sh
+++ deep-learning/scripts/train_deeplab_pretrained.sh
@@ -16,7 +16,7 @@ conda activate $PYTORCH
 echo "== This is the scripting step! =="
 
 sleep 30
-
-python /projects/kadh5719/image-segmentation_antarctica/deep-learning/scripts/train_deeplab_pretrained.py --lr 0.0001 --train-batch-size 64 --restart-checkpoint True --checkpoint-path /projects/kadh5719/image-segmentation_antarctica/deep-learning/models/session_0/deeplabv3_pretrained---bn2d-38.pth --checkpoint-prefix deeplabv3-s038-sgd-4-0.9-4
+# lr-model-momentum-weightdecay
+python /projects/kadh5719/image-segmentation_antarctica/deep-learning/scripts/train_deeplab_pretrained.py --lr 0.00001 --train-batch-size 64 --restart-checkpoint True --checkpoint-path /projects/kadh5719/image-segmentation_antarctica/deep-learning/models/deeplabv3-s038-sgd-4-0.9-4/deeplabv3-s038-sgd-4-0.9-4-28.pth --checkpoint-prefix deeplabv3-sgd-4-0.9-4-28-sgd-4-0.9-4
 
 echo "== End of Job =="
